370fbef586dc84927ffcaf59f9f36a53a72b4350
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-08-20 23:28:47 +0900

    feat: 프로젝트 초기 설정 (#1)

---
+++ src/test/java/com/example/talkenbackend/TalkenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talkenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
